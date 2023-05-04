36ad0b19ca8afdb3b00da4bda0de0d2dafa7eb6a
Hieu Nguyen <hieunv@live.com>
2023-05-04 15:51:45 +0700

    config fluentui and tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'srv-mnt-app';
+
+  exampleTextField = '';
+
+  onClick() {
+    console.log(this.exampleTextField);
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  provideFluentDesignSystem,
+  fluentCard,
+  fluentButton,
+  fluentTextField,
+  fluentSelect,
+  fluentOption,
+  fluentNumberField,
+} from '@fluentui/web-components';
 
 import { AppModule } from './app/app.module';
 
+provideFluentDesignSystem().register(
+  fluentCard(),
+  fluentButton(),
+  fluentTextField(),
+  fluentSelect(),
+  fluentOption(),
+  fluentNumberField()
+);
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
